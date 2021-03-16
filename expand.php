b9435d2c3d993f538a35c4812b53ffe68e4f05d2
26thmeusoc <3766223+26thmeusoc@users.noreply.github.com>
2021-03-16 21:34:53 +0100

    Add an Remove-Button

--- expand.php
+++ expand.php
@@ -11,5 +11,5 @@
     $row = $result->fetch();
     echo "<tr id='commentbox".$_GET["id"]."' class='commentbox'><td colspan=3 class='commenttext'>";
     echo $row['comment'];
-    echo "</td><td><a href='./edit.php?id=".$_GET["id"]."'><div class='editbutton'>Edit</div></a></td></tr>";
+    echo "</td><td style='display:flex;'><a href='./edit.php?id=".$_GET["id"]."'><div class='editbutton'>Edit</div><a href='./remove.php".$_GET["id"]."'><div class='removebutton'>Remove</div></div></a></td></tr>";
 ?>
