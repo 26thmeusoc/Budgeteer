bbeb36ae6e3f453b344b974f4b396e6ed266605c
26thmeusoc <3766223+26thmeusoc@users.noreply.github.com>
2021-03-16 12:45:47 +0100

    Add preliminary Code

---
+++ expand.php
@@ -0,0 +1,15 @@
+<?php
+    include("data/access.php");
+    $db = openDB('data/budgeteer.sqlite3');
+    
+    $query = "SELECT comment FROM purchase WHERE id=".$_GET["id"]; 
+    try {
+        $result = $db->query($query); // Execute it
+    } catch (PDOException $e) {
+        echo "Error ".$e->getCode()."! Last Message was:<br/>".$e->getMessage()."<br/> Call was: ".$query;
+    }
+    $row = $result->fetch();
+    echo "<tr><td colspan=3>";
+    echo $row['comment'];
+    echo "</td><td><a href='./edit.php?id=".$_GET["id"]."'><div class='editbutton'>Edit</div></a></td></tr>";
+?>
